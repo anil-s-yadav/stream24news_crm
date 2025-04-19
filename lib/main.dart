ecc7c4281b7edbf4cf82a98ed8733b5d625e47e4
anil-s-yadav <paytmking15@gmail.com>
2025-04-19 18:09:17 +0530

    error fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:stream24news_crm/screens/home_screen.dart';
@@ -5,12 +7,39 @@ import 'firebase_options.dart';
 import 'screens/login_screen.dart';
 import 'theme/app_theme.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+Future<void> main() async {
+  try {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    runApp(const MyApp());
+  } catch (e) {
+    log('Error initializing app: $e');
+    runApp(const ErrorApp());
+  }
+}
+
+class ErrorApp extends StatelessWidget {
+  const ErrorApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Stream24 News CRM',
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.darkTheme(),
+      home: const Scaffold(
+        body: Center(
+          child: Text(
+            'Error initializing application.\nPlease check your connection and refresh.',
+            style: TextStyle(color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
