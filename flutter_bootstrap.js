9180cff04670d0da2cce0fb3d063ebe1793b4866
anil-s-yadav <paytmking15@gmail.com>
2025-04-25 12:25:50 +0530

    Update Flutter web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2128396771"
+    serviceWorkerVersion: "3131795151"
   }
 });
